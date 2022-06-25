a2d987467d50c201e61e61fe774068f5c11882b4
lyjguy <lyjguy@naver.com>
2022-06-25 09:10:25 +0900

    user repository 추가

---
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.lyjguy.kotlinspringjdsl.repository
+
+import com.lyjguy.kotlinspringjdsl.model.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<User, Long> {
+}
