dcacb937ed15e1d1686d15ccc59d736e0623c9ee
lyjguy <lyjguy@naver.com>
2022-07-01 08:56:27 +0900

    UserControllerTest 추가

---
+++ src/test/kotlin/com/lyjguy/kotlinspringjdsl/controller/UserControllerTest.kt
@@ -0,0 +1,14 @@
+package com.lyjguy.kotlinspringjdsl.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Test
+    fun getUser() {
+    }
+}
