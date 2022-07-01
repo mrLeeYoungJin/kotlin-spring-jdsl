d0e4d88ba150be7965e75bf34ffbf367be1023d2
lyjguy <lyjguy@naver.com>
2022-07-02 01:13:45 +0900

    UserController 수정

--- src/main/kotlin/com/lyjguy/kotlinspringjdsl/controller/UserController.kt
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/controller/UserController.kt
@@ -3,17 +3,19 @@ package com.lyjguy.kotlinspringjdsl.controller
 import com.lyjguy.kotlinspringjdsl.model.entity.User
 import com.lyjguy.kotlinspringjdsl.service.UserService
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/users")
 class UserController(
     private val userService: UserService,
 ) {
+
+    @PostMapping
+    fun createUser(): ResponseEntity<Unit> {
+        return ResponseEntity.ok(userService.save())
+    }
 
     @GetMapping("/{id}")
     fun getUser(@PathVariable id: Long): ResponseEntity<User?> {
