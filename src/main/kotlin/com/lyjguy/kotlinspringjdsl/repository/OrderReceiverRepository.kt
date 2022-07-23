2b37a7e1bba1a2b480d893e481359f1c72632767
lyjguy <lyjguy@naver.com>
2022-07-23 14:01:11 +0900

    OrderReceiverRepository findByOrderId 함수 추가

--- src/main/kotlin/com/lyjguy/kotlinspringjdsl/repository/OrderReceiverRepository.kt
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/repository/OrderReceiverRepository.kt
@@ -4,4 +4,5 @@ import com.lyjguy.kotlinspringjdsl.model.entity.OrderReceiver
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface OrderReceiverRepository: JpaRepository<OrderReceiver, Long> {
+    fun findByOrderId(orderId: Long): OrderReceiver?
 }
