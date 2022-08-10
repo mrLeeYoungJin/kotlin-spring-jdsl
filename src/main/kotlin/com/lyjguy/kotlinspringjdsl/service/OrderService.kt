8a6c49a529fcc71f19c556f2feb577e12a68681b
lyjguy <lyjguy@naver.com>
2022-08-10 13:17:08 +0900

    OrderService findById 함수 추가

--- src/main/kotlin/com/lyjguy/kotlinspringjdsl/service/OrderService.kt
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/service/OrderService.kt
@@ -1,17 +1,34 @@
 package com.lyjguy.kotlinspringjdsl.service
 
+import com.linecorp.kotlinjdsl.querydsl.expression.col
+import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
+import com.linecorp.kotlinjdsl.spring.data.singleQuery
 import com.lyjguy.kotlinspringjdsl.model.dto.ReqOrderDto
 import com.lyjguy.kotlinspringjdsl.model.entity.Order
 import com.lyjguy.kotlinspringjdsl.model.entity.OrderReceiver
+import com.lyjguy.kotlinspringjdsl.model.entity.User
 import com.lyjguy.kotlinspringjdsl.repository.OrderRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import javax.persistence.NoResultException
 
 @Service
 class OrderService(
     private val orderRepository: OrderRepository,
     private val orderReceiverService: OrderReceiverService,
+    private val queryFactory: SpringDataQueryFactory,
 ) {
+    fun findById(id: Long): Order? {
+        return try {
+            queryFactory.singleQuery {
+                select(entity(Order::class))
+                from(entity(Order::class))
+                where(col(Order::id).equal(id))
+            }
+        } catch (nre: NoResultException) {
+            null
+        }
+    }
 
     @Transactional
     fun save(reqOrderDto: ReqOrderDto) {
