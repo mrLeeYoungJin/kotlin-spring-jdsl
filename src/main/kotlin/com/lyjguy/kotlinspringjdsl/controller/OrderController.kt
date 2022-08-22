e6c007f19f370509268be239f83db171ea35554d
lyjguy <lyjguy@naver.com>
2022-08-22 13:32:57 +0900

    getOrder api 추가

--- src/main/kotlin/com/lyjguy/kotlinspringjdsl/controller/OrderController.kt
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/controller/OrderController.kt
@@ -1,6 +1,7 @@
 package com.lyjguy.kotlinspringjdsl.controller
 
 import com.lyjguy.kotlinspringjdsl.model.dto.ReqOrderDto
+import com.lyjguy.kotlinspringjdsl.model.entity.Order
 import com.lyjguy.kotlinspringjdsl.service.OrderService
 import org.springframework.web.bind.annotation.*
 
@@ -13,5 +14,10 @@ class OrderController(
     @PostMapping
     fun savePost(@RequestBody reqOrderDto: ReqOrderDto) {
         orderService.save(reqOrderDto)
+    }
+
+    @GetMapping
+    fun getOrder(@PathVariable orderId: Long): Order? {
+        return orderService.findById(orderId)
     }
 }
