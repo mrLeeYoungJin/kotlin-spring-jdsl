37908e64b334b6dc39cc1d259dcca68e9e7cf50a
lyjguy <lyjguy@naver.com>
2022-08-24 13:49:58 +0900

    swagger 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:2.0.3.RELEASE")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
 
     // Querydsl
     implementation("com.querydsl:querydsl-jpa")
