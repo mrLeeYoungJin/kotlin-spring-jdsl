3b94ce64df25fcf5b8fe6d08c6278a58aa961a25
lyjguy <lyjguy@naver.com>
2022-09-03 19:32:27 +0900

    OrderReceiverController swagger 적용

--- src/main/kotlin/com/lyjguy/kotlinspringjdsl/controller/OrderReceiverController.kt
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/controller/OrderReceiverController.kt
@@ -2,14 +2,17 @@ package com.lyjguy.kotlinspringjdsl.controller
 
 import com.lyjguy.kotlinspringjdsl.model.dto.ResOrderReceiverDto
 import com.lyjguy.kotlinspringjdsl.service.OrderReceiverService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/order_receivers")
+@Api(description = "주문 수령자 API", tags = ["주문 수령자 api"])
 class OrderReceiverController(
     val orderReceiverService: OrderReceiverService
 ) {
-
+    @ApiOperation(value = "주문 수령자 조회")
     @GetMapping("/{id}")
     fun getOrderReceiver(@PathVariable("id") id : Long): ResOrderReceiverDto? {
         return orderReceiverService.findById(id)
