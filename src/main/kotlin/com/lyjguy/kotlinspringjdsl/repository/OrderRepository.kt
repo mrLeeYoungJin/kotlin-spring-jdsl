76d77bec0928337dbe6d82a31bc6d94f41f2ce1d
lyjguy <lyjguy@naver.com>
2022-10-09 08:32:22 +0900

    userId 기반 주문 정보 조회 쿼리 추가

--- src/main/kotlin/com/lyjguy/kotlinspringjdsl/repository/OrderRepository.kt
+++ src/main/kotlin/com/lyjguy/kotlinspringjdsl/repository/OrderRepository.kt
@@ -3,4 +3,6 @@ package com.lyjguy.kotlinspringjdsl.repository
 import com.lyjguy.kotlinspringjdsl.model.entity.Order
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrderRepository: JpaRepository<Order, Long>
+interface OrderRepository: JpaRepository<Order, Long> {
+    fun findAllByUserId(userId: Long): List<Order>
+}
